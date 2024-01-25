261938b4b05a0f9de26a9126aa2af7c4d403fa5c
jirrazabal <joseirrazabal@gmail.com>
2024-01-25 16:01:41 -0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -13,9 +13,9 @@ const getServer = async () => {
   const app = express();
   const server = createServer(app);
 
+  app.use(cors());
   app.use(morgan("dev"));
   app.use(helmet());
-  app.use(cors('*'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
