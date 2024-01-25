c756a5e37ae2df44773ca552f26414cb373cc386
jirrazabal <joseirrazabal@gmail.com>
2024-01-25 13:41:45 -0300

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
+  PORT: process.env.PORT,
   GOOGLE_APPLICATION_CREDENTIALS_JSON:
     (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON &&
       JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)) ||
