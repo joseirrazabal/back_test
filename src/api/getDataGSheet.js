14d49a39c1d152b9235ba4c9bf9757ac520e7313
Luciano Recchini <lucianorecchini@MacBook-Air-de-Natalia.local>
2024-10-24 12:40:23 -0300

    registro 2

--- src/api/getDataGSheet.js
+++ src/api/getDataGSheet.js
@@ -1,34 +1,40 @@
-import { google } from "googleapis";
+import { google } from 'googleapis';
+import config from '../config'; // Importamos la configuración
 
-const readGoogleSheet = async (credentials, spreadsheetId, range) => {
+const readGoogleSheet = async (spreadsheetId, range) => {
+  // Configuración de GoogleAuth usando el contenido de las credenciales JSON desde una variable de entorno
   const auth = new google.auth.GoogleAuth({
-    credentials,
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON), // Aquí utilizas directamente el contenido JSON de las credenciales
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
 
   const client = await auth.getClient();
 
-  const googleSheets = google.sheets({ version: "v4", auth: client });
+  const googleSheets = google.sheets({ version: 'v4', auth: client });
 
-  const response = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range,
-  });
-
-  const rows = response.data.values;
-  if (rows.length) {
-    const headers = rows.shift();
-
-    return rows.map((row) => {
-      let obj = {};
-      headers.forEach((header, index) => {
-        obj[header] = row[index];
-      });
-      return obj;
+  try {
+    const response = await googleSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
     });
-  } else {
-    console.log("No data found.");
+
+    const rows = response.data.values;
+
+    if (rows && rows.length) {
+      const headers = rows.shift();
+      return rows.map((row) => {
+        let obj = {};
+        headers.forEach((header, index) => {
+          obj[header] = row[index];
+        });
+        return obj;
+      });
+    } else {
+      console.log('No data found.');
+      return [];
+    }
+  } catch (error) {
+    console.error("Error while reading Google Sheet:", error.message);
     return [];
   }
 };
