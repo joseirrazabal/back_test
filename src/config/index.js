076f294c7b095d4f737fbf7a639624e8b3f55771
jirrazabal <joseirrazabal@gmail.com>
2024-10-28 19:27:06 -0300

    fix port

--- src/config/index.js
+++ src/config/index.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  PORT: process.env.PORT || 5000, // Cambia a un puerto diferente, por ejemplo 5000
-  GOOGLE_APPLICATION_CREDENTIALS_JSON: (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON &&
-    JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)) ||
+  PORT: process.env.PORT || 3001,
+  GOOGLE_APPLICATION_CREDENTIALS_JSON:
+    (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON &&
+      JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)) ||
     "",
 };
 
-export default config
+export default config;
