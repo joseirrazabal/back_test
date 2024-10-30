0e6db28794bf068f04ac2edf6a50c2d1ce771643
Luciano Recchini <lucianorecchini@MacBook-Air-de-Natalia.local>
2024-10-29 23:35:17 -0300

    agregados 3

--- src/config/index.js
+++ src/config/index.js
@@ -4,11 +4,11 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3001,
-  GOOGLE_APPLICATION_CREDENTIALS_JSON: {
+  googleCredentials: {
     type: process.env.GOOGLE_TYPE,
     project_id: process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza \n por saltos de línea reales
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
     auth_uri: process.env.GOOGLE_AUTH_URI,
